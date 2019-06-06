6f6ad57a06ea044c90c5eb55573b583fd7caad47
chumpon-asaneerat <chumponsenate@gmail.com>
2019-06-06 20:14:21 +0700

    Add class implements guideline #1.

--- nlib-dom-json.js
+++ nlib-dom-json.js
@@ -1,3 +1,16 @@
+// - Class to create HTML Tag element.
+
+// - Class to wrap HTML individual Tags.
+
+// - Class mixin methods.
+
+// - Class to access HTML dom styles.
+
+// - Class to access HTML dom attributes.
+
+// - Class to access HTML dom events.
+
+
 (() => {
     console.log('nlib loaded.');
 })();
